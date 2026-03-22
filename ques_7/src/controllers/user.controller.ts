4e43a88534450673e331cb6bd4c2ab6d92a4f42c
Ritik <ritikyadav2426@gmail.com>
2026-03-22 22:35:05 +0530

    ques 7 done

---
+++ ques_7/src/controllers/user.controller.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { createUser, getUser} from "../services/user.services";
+
+
+export  const userInfo = (req: Request , res: Response) => {
+
+    const {name, email} = req.body;
+
+    const result = createUser({name , email});
+
+    res.status(200).json(result);
+    
+}
+
+export const searchUser = (req: Request , res: Response , next: NextFunction) => {
+
+    try{
+
+        throw new Error("Forced error just to test middleware");
+        const limit = Number(req.query.limit);
+        const q = String(req.query.q);
+        
+        
+        const result = getUser(q , limit);
+        
+        if(result === undefined){
+            
+            return res.status(404).send("user not found");
+        }
+        
+        return res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+    
