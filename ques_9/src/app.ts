ab5bc126320c6be7656b26f3f000929c6ef562bd
Ritik <ritikyadav2426@gmail.com>
2026-03-24 21:42:34 +0530

    done

--- ques_9/src/app.ts
+++ ques_9/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/user.routes'
-import {authMiddleware} from './middlewares/user.middleware';
+import { corsConfigBrowser , corsConfigPostman} from './middlewares/user.middleware';
 
 const app = express();
 
 
 app.use(express.json());
-app.use(authMiddleware);
+
+app.use(corsConfigPostman);
 
 app.use('/api/users' , userRoutes);
 
